2cb7a76759b344e164671041da6a29e86be2d486
priyambuilds <dpriyam45@gmail.com>
2026-09-23 13:18:58 +0530

    update

--- ticket-booking-system-prisma/src/controller/services/transactionService.ts
+++ ticket-booking-system-prisma/src/controller/services/transactionService.ts
@@ -1,3 +1,12 @@
-export async function getTransactionService() {
-    
+import { prisma } from "../../../prisma/index.ts"
+
+export async function getTransactionService(userId: string, role: string) {
+    if (role === "ADMIN") {
+        return prisma.transaction.findMany({})
+    } else {
+        return prisma.transaction.findMany({
+            where: { userId },
+            orderBy: { createdAt: "desc" },
+        })
+    }
 }
--- ticket-booking-system-prisma/src/controller/transactionController.ts
+++ ticket-booking-system-prisma/src/controller/transactionController.ts
@@ -1,8 +1,16 @@
 import type { Request, Response, NextFunction } from "express";
+import { getTransactionService } from "./services/transactionService";
 
 export async function transactionController(req: Request, res: Response, next: NextFunction) {
     try {
-
+        const userId = req.userId
+        const role = req.role
+        const transaction = await getTransactionService(userId, role)
+        res.status(200).json({
+            success: true,
+            message: "Below are all the transactions",
+            data: [{transaction}]
+        })
     } catch (e) {
         next(e)
     }
